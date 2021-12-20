c501dc3a260dd96a07243b08cdaffc4994f77ce9
codeKAMAT <easykamat@gmail.com>
2021-12-21 01:02:46 +0530

    adding bootstrap Navbar and toggle functionality w.t.h of useState

---
+++ src/Navbar.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+
+const Navbar = () => {
+    const [show, setShow] = useState(false)
+    return (
+        <>
+          <section className='navbar-bg'>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+                <a className="navbar-brand" href="#">KamatPAY</a>
+                <button className="navbar-toggler" onClick={() => setShow(!show)} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className={`collapse navbar-collapse ${show ? "show": ""}`}>
+                <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">Services</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">About</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">Contact</a>
+                    </li>                    
+                </ul>
+                <form className="d-flex">
+                    <button className="btn btn-outline-success" type="submit">Sign Up</button>
+                    <button className="btn btn-outline-success" type="submit">Log in</button>
+
+                </form>
+                </div>
+            </div>
+            </nav>
+           </section>
+        </>
+    )
+}
+
+export default Navbar
