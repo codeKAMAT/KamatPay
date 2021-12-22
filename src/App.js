6797ba823ef6df610bd004c5dbca458333c36dc2
codeKAMAT <easykamat@gmail.com>
2021-12-22 19:32:53 +0530

    adding HowItWorks.js component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Header from './Header'
+import HowItWorks from './HowItWorks'
 const App = () => {
   return (
     <>
       <Navbar/>
       <Header/>
+      <HowItWorks/>
     </>
   )
 }
